339c05d38320e78e35d32d828ed3a007d1d59ef8
HandsomeSphe <siphelelehandsome>
2023-05-02 12:17:55 +0200

    minor changes and fixes

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const small_nav_lines = document.querySelectorAll(".smallNav__menu--line");
 const small_nav_links = document.querySelector(".smallNav__list");
 
 const showRemoveIcon = () => {
+  console.log("Clicked");
   small_nav_lines[1].classList.toggle("removeMiddle");
   small_nav_lines[0].classList.toggle("rotateDown");
   small_nav_lines[2].classList.toggle("rotateUp");
 };
 
 small_nav_menu.addEventListener("click", (e) => {
+  e.preventDefault();
   showRemoveIcon();
   small_nav_links.classList.toggle("revealLinks");
 });
 
-/**** Slider from Tailwind****/
+// /**** Slider from Tailwind****/
 
-// Initialization for ES Users
-import { Carousel, initTE } from "tw-elements";
-initTE({ Carousel });
+// // Initialization for ES Users
+// import { Carousel, initTE } from "tw-elements";
+// initTE({ Carousel });
