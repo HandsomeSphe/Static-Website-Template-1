21ef4f1c6c5232a2a70cd43c9b76fc2de277d642
HandsomeSphe <siphelelehandsome>
2023-05-02 11:09:43 +0200

    added all files

---
+++ script.js
@@ -0,0 +1,14 @@
+const small_nav_menu = document.querySelector(".smallNav__menu");
+const small_nav_lines = document.querySelectorAll(".smallNav__menu--line");
+const small_nav_links = document.querySelector(".smallNav__list");
+
+const showRemoveIcon = () => {
+  small_nav_lines[1].classList.toggle("removeMiddle");
+  small_nav_lines[0].classList.toggle("rotateDown");
+  small_nav_lines[2].classList.toggle("rotateUp");
+};
+
+small_nav_menu.addEventListener("click", (e) => {
+  showRemoveIcon();
+  small_nav_links.classList.toggle("revealLinks");
+});
